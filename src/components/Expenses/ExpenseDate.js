11a1b81b9d13f00179073dc3932256700da798ea
keatonjwhitehead <keaton.j.whitehead@gmail.com>
2021-11-30 17:30:43 -0700

    Using one state instead of many

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ExpenseDate.css';
+
 function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,21 +3,38 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: '',
+    });
 
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-        console.log(event.target.value);
+        // setEnteredTitle(event.target.value);
+        // console.log(event.target.value);
+        setUserInput({
+            ...userInput, // This copies the previous state of information the other object data is not overwritten
+            enteredTitle: event.target.value // this overrides just the previous element that was changed from the new retrieved input
+        })
     };
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-        console.log(event.target.value);
+        // setEnteredAmount(event.target.value);
+        // console.log(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value
+        })
     };
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-        console.log(event.target.value);
+        // setEnteredDate(event.target.value);
+        // console.log(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        })
     };
   return (
     <form>
