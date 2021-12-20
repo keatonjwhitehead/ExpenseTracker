ce5d41320c09b40fa19c5c88c5a288960dc1a021
keatonjwhitehead <keaton.j.whitehead@gmail.com>
2021-12-20 10:44:10 -1000

    updated code with instructor code because we lost the working version :(

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,64 +1,34 @@
-import React, {useState} from 'react';
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from 'react';
+
 import Card from '../UI/Card';
-import "./Expenses.css";
-import ExpenseFilter from "./ExpensesFilter";
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
 
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    //Creating an intial state to listen to the year selected from the user from the drop down menu. 
-    //Value gets stored in the enteredYear and the function setEnteredYear is called
-    const [enteredYear, setEnteredYear] = useState('2022');
-    const filterYearSelected = selectedYear => {
-      setEnteredYear(selectedYear);
-      console.log(selectedYear);
-    }
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
-    <Card className="expenses">
-      <ExpenseFilter selected={enteredYear} onUpdatedYear = {filterYearSelected} />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-      <ExpenseItem
-        title={props.items[4].title}
-        amount={props.items[4].amount}
-        date={props.items[4].date}
-      />
-      <ExpenseItem
-        title={props.items[5].title}
-        amount={props.items[5].amount}
-        date={props.items[5].date}
-      />
-       <ExpenseItem
-        title={props.items[6].title}
-        amount={props.items[6].amount}
-        date={props.items[6].date}
-      />
-      <ExpenseItem
-        title={props.items[7].title}
-        amount={props.items[7].amount}
-        date={props.items[7].date}
-      />
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onUpdatedYear={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
   );
-}
+};
+
 export default Expenses;
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,82 +1,94 @@
-import React, {useState} from "react";
-import "./ExpenseForm.css";
+import React, { useState } from 'react';
+
+import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
-  
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: '',
-    //     enteredAmount: '',
-    //     enteredDate: '',
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
     // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
 
-    const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-        // console.log(event.target.value);
-        // setUserInput({
-        //     ...userInput, // This copies the previous state of information the other object data is not overwritten
-        //     enteredTitle: event.target.value, // this overrides just the previous element that was changed from the new retrieved input
-        // })
-        // If your current state depends on the previous state it is better to use the fucntion below:
-        // setUserInput((prevState) => {
-        //     return {...prevState,enteredTitle: event.target.value};
-        // });
-    };
-    const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-        // console.log(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredAmount: event.target.value,
-        // })
-    };
-    const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-        // console.log(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredDate: event.target.value,
-        // })
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
     };
 
-    //When you submit a form the html normally restarts the page because it sends the information to the hosting server, we don't
-    //want this. We wan to keep it internal and send it to be manipulated with Javascript.
-    const submitHandler = (event) => {
-        event.preventDefault();
-
-        const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
-        };
-
-        props.onSaveExpenseData(expenseData);
-        setEnteredTitle('');
-        setEnteredAmount('');
-        setEnteredDate('');
-    };
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label> Title</label>
-          <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
-        <div className="new-expense__control">
-          <label> Amount</label>
-          <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
-        <div className="new-expense__control">
-          <label> Date</label>
-          <input type="date" min="2020-01-01" max="2025-12-31" value={enteredDate} onChange={dateChangeHandler} />
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+      <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type='submit'>Add Expense</button>
       </div>
     </form>
   );
